4be8a0c803f34c207d126d3310744fb1aa99e56f
lucamufe <luiscarlos.munozf@outlook.com>
2020-07-20 19:03:24 -0500

    DataScience: Generar Tabla

--- scripts/master.js
+++ scripts/master.js
@@ -11,7 +11,7 @@ function agregarHeader(){
           '<li class="logo"><a href="/"><img src="/imagenes/Logo1.PNG" alt="Cursos y Tips" width="80" height="47"'+ 'class="imgLogo"></a></i>'+
           '<li><a href="/">Excel</a></li>'+
           '<li><a href="/temas/powerbi/powerbi.html">PowerBI</a></li>'+
-          '<li><a href="/temas/python/python.html">Python</a></li>'+
+          '<li><a href="/temas/python/python.html">Data Science</a></li>'+
         '</ul>'+
       '</nav>'+
       '<button><a href="/temas/contacto/contacto.html">Contacto</a></button>'
@@ -21,5 +21,5 @@ function agregarFooter(){
     var footer = document.getElementById("footer");
     footer.innerHTML = 
         '<h4>Cursos y Tips</h4>'+
-        '<p>Si no encuentra el tema que está buscando, solicitarlo a través de la sección <a href="/temas/contacto/contacto.html">CONTACTO</a>.</p>'
+        '<p>Si no encuentra el tema que está buscando, solicitarlo a través de la sección <a href="/temas/contacto/contacto.html"><u>CONTACTO</u></a>.</p>'
 }
